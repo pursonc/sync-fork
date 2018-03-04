74f2e9877e020b7119d4ee41ea5c035dea7e3e42
Purson Chen <purson.chen@gmail.com>
2018-03-04 15:00:00 +0800

    resolve the compatibility problem

--- README.md
+++ README.md
@@ -4,8 +4,14 @@
 Syncing a Fork with the main repository
 
 # Installation
-* Check if installed jq(the command line tools for json parse)
 * Copy the sh to your local repository root directory.
-* sh ./sync-local-upstream.sh
 
-#test
+# Usage
+* sh ./sync-local-upstream.sh [YOUR_FORK_ROOT]
+* It is good for cooperate with crontab.
+
+# Attention
+* It is suitable for the nodejs project that included the "package.json".
+* If your fork is the nodejs project that you will need jq installed at first.
+* Otherwise your fork is not the nodejs project, it will require the source repository url input.
+
--- sync-local-upstream.sh
+++ sync-local-upstream.sh
@@ -1,28 +1,46 @@
 # Assumes that you've committed your work on your current branch
 # 
 # OSX must be installed brew first
-# 
 # Debian and Ubuntu repositories. Install using sudo apt-get install jq.
 # Fedora repository. Install using sudo dnf install jq.
 # openSUSE repository. Install using sudo zypper install jq.
 # repository. Install using sudo pacman -Sy jq.
 # 
-# Verify that the command line Json parser is installed.
-# 
-if hash jq 2>/dev/null;
-    then
-       echo "jq installed!!"
-    else
-       if hash brew 2>/dev/null;
-         then
-            brew install jq
-         else
-            echo "Please refer to the comment installation for the jq tool in the corresponding OS."
-       fi
-fi
+ 
+function sync_fork() {
+
+orgin_git=""
+
+DIRFILES=`ls $1`
+if [[ $DIRFILES =~ "package.json" ]]
+then
+    # Verify that the command line Json parser is installed.
+    if hash jq 2>/dev/null;
+        then
+           echo "jq installed!!"
+        else
+           if hash brew 2>/dev/null;
+             then
+                brew install jq
+             else
+                echo "Please refer to the comment installation for the jq tool in the corresponding OS."
+           fi
+    fi
+    
+    #Get the source address in the package. Json.
+    orgin_git=$(cat package.json | jq '.repository.url')
+    orgin_git=${orgin_git##*+}
+    orgin_git=${orgin_git%%"\""}
+  else
+     echo -n  "please enter the source repository url ->  "
+     read  orgin_git
+     
+ fi
+
+
 
 #Syncing a Fork with the main repository
-function sync_fork() {
+function sync_source() {
    current_branch=$(git rev-parse --abbrev-ref HEAD)
   
    git fetch upstream
@@ -32,10 +50,6 @@ function sync_fork() {
    git checkout $current_branch
 }
 
-#Get the source address in the package. Json.
-orgin_git=$(cat package.json | jq '.repository.url')
-orgin_git=${orgin_git##*+}
-orgin_git=${orgin_git%%"\""}
 
 #Check if the local repository exists upstream.
 #If it exists, it is in direct sync.
@@ -44,8 +58,11 @@ my_remote_repository=$(git remote -v)
 echo $my_remote_repository
 if [[ $my_remote_repository =~ "upstream" ]]
 then
-   sync_fork
+   sync_source
 else
    git remote add upstream $orgin_git
-   sync_fork
+   sync_source
 fi
+}
+
+sync_fork
